c57c10f1aa4228202513ab2c7bf8007100e15cc7
pornprapa-ron <pornprapa.ron@gmail.com>
2021-01-24 18:31:30 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -10,18 +10,21 @@ function App() {
       <div class="col-4 border border-info">Contents 1 : col-xs-4</div>
       <div class="col-4 border border-info">Contents 2 : col-xs-4</div>
       <div class="col-4 border border-info">Contents 3 : col-xs-4</div>
+      <div class="col-4 border border-info">Contents 4 : col-xs-4</div>
     </div>
 
     <div class="row">
       <div class="col-md-4 border border-primary">Contents 1 : col-md-4</div>
       <div class="col-md-4 border border-primary">Contents 2 : col-md-4</div>
       <div class="col-md-4 border border-primary">Contents 3 : col-md-4</div>
+      <div class="col-md-4 border border-primary">Contents 4 : col-md-4</div>
     </div>
 
     <div class="row">
       <div class="col-lg-4 border border-danger">Contents 1 : col-lg-4</div>
       <div class="col-lg-4 border border-danger">Contents 2 : col-lg-4</div>
       <div class="col-lg-4 border border-danger">Contents 3 : col-lg-4</div>
+      <div class="col-lg-4 border border-danger">Contents 4 : col-lg-4</div>
     </div>
 
     </div>
